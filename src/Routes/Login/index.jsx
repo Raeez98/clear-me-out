80fa797375b8179b1cf6b7ac9d5c04815791ae0f
Raeez98 <raeezmohammed97@gmail.com>
2021-12-04 12:01:20 +0530

    4/12/21

--- src/Routes/Login/index.jsx
+++ src/Routes/Login/index.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import "./login.css"
+
 
 const Login = () => {
     return (
         <div>
             <h1>Login</h1>
+            <form>
+                <input type ="text"  placeholder="Email" /><br/>
+                <input type ="text"  placeholder="password" /><br/>
+               <input type="submit" value="Submt"/>
+               </form>
+
         </div>
     )
 }
--- src/Routes/Signup/index.jsx
+++ src/Routes/Signup/index.jsx
@@ -1,10 +1,49 @@
-import React from 'react'
+import React, { useState } from 'react'
+import "./signup.css"
 
 const Signup = () => {
+    const[formData,setFormData]=useState({
+        fullName: "",
+        email: "",
+        password: "",
+        confirmpassword: ""
+   });
+  
+   const{fullName,email,password,confirmpassword}=formData;
+   
     return (
-        <div>
-            <h1>Sign up</h1>
+        <div className="signupcontainer-page">
+          <div className="sign-up-contanier">
+            <div className="signing-title">Sign Up</div>
+            <form>
+                <div className="inputform"> 
+                    <input type="text" 
+                    value={fullName}
+                    onChange={(e)=>{
+                            setFormData({
+                            ...formData,
+                            fullName:e.target.value
+                        })
+                    }}
+                    />  
+                    <div className="inputfield">FullName</div>   
+                </div>
+                <div className="inputform">
+                    <input type="text"/>    
+                    <div className="inputfield">Email</div> 
+                </div>
+                <div className="inputform">
+                     <input type="text"/>    
+                    <div className="inputfield">Password</div>
+                </div>
+                <div className="inputform">
+                    <input type="text"/>      
+                    <div className="inputfield">Confirm Password</div>
+                </div>
+                <button className="signing-button">Submit</button>
+            </form>
         </div>
+    </div>
     )
 }
 
