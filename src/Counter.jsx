4af8dc0d74702989ce4b02f08f3f4816cedcdfcd
Raeez98 <raeezmohammed97@gmail.com>
2021-12-09 16:39:54 +0530

    todlist sorting and adding values using pure redux

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -1,30 +1,89 @@
 import React from 'react'
 import { createStore } from 'redux' 
 import "./counter.css"
+import { useState } from 'react'
 
 
 const Counter = ({store}) => {
-    return (
-        <div className="main_container_count">
-            <button onClick={()=>{
-                store.dispatch({
-                    type:"Increment"
-                })
-            }}
-             className="increment">+</button>
+    const[input,setInput]=useState("");
 
-            <div className="counter-result">
-                {store.getState()}</div>
-
-            <button onClick={()=>{
-                store.dispatch({
-                    type:"Decrement"
+            const addTodo=(value)=>{
+                store.dispatch(
+                    {
+                        type:"ADD_TODO",
+                        value
+                    })
+            }
+            const deleteTodo=(index)=>{
+            store.dispatch(
+                {
+                    type:"DELETE_TODO",
+                    index
                 })
-            }}
+        }
+        const sortTodo=(value)=>{
+            store.dispatch(
+                {
+                    type:"SORT_TODO",
+                    value
+                })
+        }
+
+
+            return(
+                <div className="toDo">
+                    <div className="input-section">
+                        <input type="text"
+                        onChange={(e)=>{
+                            setInput(e.target.value)
+                        }
+                        }/>
+                        <button onClick={
+                            ()=>{
+                                addTodo(input);
+                            }
+                        }>Add</button>
+                    </div>
+                    <div className="todo-list">
+                        {
+                            store.getState().map((value,i)=><div key={i}>
+                                {value}
+                                <button onClick={()=>{
+                                   deleteTodo(i)
+                                }}>close</button></div>)
+                        }
+                    </div>
+                    <button onClick={()=>{
+                        sortTodo(input);
+
+                    }
+                    }>SORT</button>
+                </div>
+    
+        // <div className="main_container_count">
+        //     <button onClick={()=>{
+        //         store.dispatch({
+        //             type:"Increment"
+        //         })
+        //     }}
+        //      className="increment">+</button>
+
+        //     <div className="counter-result">
+        //         {store.getState()}</div>
+
+        //     <button onClick={()=>{
+        //         store.dispatch({
+        //             type:"Decrement"
+        //         })
+        //     }}
+
+        //     className="decrement">-</button>
             
-            className="decrement">-</button>
+        // </div>
+        
             
-        </div>
+        
+    
     )
 }
 
--- src/index.js
+++ src/index.js
@@ -5,14 +5,16 @@ import './index.css';
 import App from './App';
 import {createStore} from "redux"
 
-const reducer=(state=0,action)=>{            //to return new function//cant mutate only we can do reset 
+const reducer=(state=[],action)=>{            //to return new function//cant mutate only we can do reset //state=0
   switch (action.type) {
-      case 'Increment':
-        return state+1
+      case 'ADD_TODO':                    //increment
+        return [...state,action.value];
         
-      case 'Decrement':
-        return  state-1
-      
+      case 'DELETE_TODO':
+        return   state.filter((_value,index)=>action.index!==index)
+
+        case 'SORT_TODO':
+          return [...state.sort()];
       default:
           return state
 
