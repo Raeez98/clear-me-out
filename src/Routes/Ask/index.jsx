7cb5b217471bbe1e59b296f3d38ed553862fe021
Raeez98 <raeezmohammed97@gmail.com>
2021-12-13 15:51:11 +0530

    ask page designing

--- src/Routes/Ask/index.jsx
+++ src/Routes/Ask/index.jsx
@@ -1,9 +1,34 @@
 import React from 'react'
+import "./ask.css"
 
 const Ask = () => {
     return (
-        <div>
-            <h1>Ask</h1>
+<div className="category-container">
+          <div className="ask-body-container">
+            <h1>Ask Question</h1>
+            <div className="drop-down-section">
+              <div className="ask">Category</div>
+           
+               <select className="drop-box">
+               
+                   <option>Electrical</option>
+               </select>
+               </div>
+            
+            <div className="text-question-box">
+                   
+                <div className="ask-question">Question</div>
+                <textarea name="text-area"></textarea>
+               
+                
+    
+            
+            <button className="ask-submit-button">Submit</button>
+            </div>
+            
+            
+
+            </div>
         </div>
     )
 }
