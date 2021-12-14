b5d829a060f5d972fbc8116bbe972d5d364f1012
Raeez98 <raeezmohammed97@gmail.com>
2021-12-14 12:35:01 +0530

    database management using redux

--- src/index.js
+++ src/index.js
@@ -5,21 +5,28 @@ import './index.css';
 import App from './App';
 import {createStore} from "redux"
 import { Provider } from 'react-redux'
+import PostTodo from './Services/PostTodo';
 
-const reducer=(state=[],action)=>{            //to return new function//cant mutate only we can do reset //state=0
+const reducer=(state=[],action)=>{   
+               //to return new function//cant mutate only we can do reset //state=0
+               let updatedList;
   switch (action.type) {
-      case 'Add_Todo':                    //increment  state + 1;
-         return [...state,action.value]                          
+      case 'Add_Todo':                             //increment  state + 1;
+      updatedList=[...state,action.value]
+      PostTodo(updatedList)
+         return updatedList;                         
        case 'Delete_Todo':
-       return state.filter((_val,i)=> i!==action.index)
-                                         //   return [...state.sort()];
+       updatedList=state.filter((_val,i)=> i!==action.index)
+       PostTodo(updatedList)
+         return updatedList;   
+       //   return [...state.sort()];
+       case 'create':
+         return action.data;
       default:
         return state
 
-                                           // case 'SORT_TODO':   
-
+                                                        // case 'SORT_TODO':   
   }
-
 }
 const store=createStore(reducer);
 
